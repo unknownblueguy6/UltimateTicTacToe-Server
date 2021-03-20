95426089bc614f48a33675ee93800de68f647ff8
unknownblueguy6 <unknownblueguy6@gmail.com>
2021-03-20 12:19:23 +0530

    Add debugging function showBoard()

--- src/game.js
+++ src/game.js
@@ -8,6 +8,7 @@ class Game extends EventEmitter{
         this.players = [];
         this.turn = 0;
         this.board = makeEmptyBoard();
+        this.showBoard()
     }
     addPlayer(player){
         this.players.push(player);
@@ -15,12 +16,35 @@ class Game extends EventEmitter{
             this.emit('started')
         }
     }
+    showBoard(){
+        for(let i = 0; i < 3; ++i){
+            for(let j = 0; j < 3; ++j){
+                let s = '';
+                for(let k = 3*j; k < 3*j + 3; ++k){
+                    s += `  ${this.board[j][k]} | ${this.board[j+1][k]} | ${this.board[j+2][k]}  `;
+                    if (k%3 < 2){
+                        s += '║';
+                    }
+                }
+                console.log(s);
+                if(j < 2)
+                    console.log(' ---+---+--- ║ ---+---+--- ║ ---+---+---');
+            }
+            if(i < 2)
+                console.log('═════════════╬═════════════╬═════════════');
+        }
+        console.log()
+    }
 }
 
 function makeEmptyBoard(){
-    let arr = Array(9);
-    arr.fill([]);
-    console.log(arr);
+    let arr = []
+    for(let i = 0; i < 9; ++i){
+        arr.push([])
+        for(let j = 0; j < 9; ++j){
+            arr[i].push(' ')
+        }
+    }
     return arr;
 }
 
